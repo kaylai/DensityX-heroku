da5727659dda26d39fff2dadd87f7adaa10014ff
Kayla Iacovino <captainkayla@gmail.com>
2017-07-03 15:39:07 -0700

    Debugging for heroku deploy

--- uploads/urls.py
+++ uploads/urls.py
@@ -9,7 +9,6 @@ from uploads.core import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
-    url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
     url(r'^admin/', admin.site.urls),
 ]
 
